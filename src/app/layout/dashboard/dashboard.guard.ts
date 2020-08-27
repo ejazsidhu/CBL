4337d9fede9216bc23e6966a2a733787b4ab24c6
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-08-27 10:25:49 +0500

    shop page accessible without login

--- src/app/layout/dashboard/dashboard.guard.ts
+++ src/app/layout/dashboard/dashboard.guard.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private location: Location) {}
 
     canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
+        // tslint:disable-next-line:triple-equals
+        if (localStorage.getItem('isLoggedin') || this.location.path().indexOf('/details/') > -1) {
             return true;
         }
 
--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
-import * as moment from "moment";
+import * as moment from 'moment';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
+    constructor(private router: Router, private location: Location) {}
 
     canActivate() {
-        
-        var t = moment(new Date).format('YYYY-MM-DD');
-        var st = localStorage.getItem('today');
+
+        const t = moment(new Date).format('YYYY-MM-DD');
+        const st = localStorage.getItem('today');
         // if (t > st) this.router.navigate(['/login']);
-        if (localStorage.getItem('isLoggedin') && t <= st) {
+        // tslint:disable-next-line:triple-equals
+        if ((localStorage.getItem('isLoggedin') && t <= st) || this.location.path().indexOf('/details/') > -1) {
             return true;
         }
 
@@ -20,3 +22,4 @@ export class AuthGuard implements CanActivate {
         return false;
     }
 }
+
