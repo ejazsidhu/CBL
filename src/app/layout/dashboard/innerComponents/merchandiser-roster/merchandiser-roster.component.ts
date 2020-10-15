d62e14081a69beb60fd009a4785f7b60ac2eface
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-10-15 19:03:51 +0500

    Total column in roster

--- src/app/layout/dashboard/innerComponents/merchandiser-roster/merchandiser-roster.component.ts
+++ src/app/layout/dashboard/innerComponents/merchandiser-roster/merchandiser-roster.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { DashboardService } from '../../dashboard.service';
-import * as moment from 'moment';
-import { environment } from 'src/environments/environment';
-import { ToastrService } from 'ngx-toastr';
-import { ExcelService } from '../../excel.service';
+import { Component, OnInit } from "@angular/core";
+import { DashboardService } from "../../dashboard.service";
+import * as moment from "moment";
+import { environment } from "src/environments/environment";
+import { ToastrService } from "ngx-toastr";
+import { ExcelService } from "../../excel.service";
 
 @Component({
-  selector: 'app-merchandiser-roster',
-  templateUrl: './merchandiser-roster.component.html',
-  styleUrls: ['./merchandiser-roster.component.scss']
+  selector: "app-merchandiser-roster",
+  templateUrl: "./merchandiser-roster.component.html",
+  styleUrls: ["./merchandiser-roster.component.scss"],
 })
 export class MerchandiserRosterComponent implements OnInit {
-
-  title = 'Merchandiser Roster';
+  title = "Merchandiser Roster";
   minDate = new Date(2000, 0, 1);
   maxDate: any = new Date();
   startDate: any = new Date();
@@ -23,7 +22,7 @@ export class MerchandiserRosterComponent implements OnInit {
   selectedRegion: any = {};
   selectedId = -1;
   loadingReportMessage = false;
-  downloadList = [{ key: 'xlsx', title: 'Excel', icon: 'fa fa-file-excel-o' }];
+  downloadList = [{ key: "xlsx", title: "Excel", icon: "fa fa-file-excel-o" }];
   selectedEvaluator: any = {};
   evaluatorList: any = [];
   userTypeId: any;
@@ -39,24 +38,29 @@ export class MerchandiserRosterComponent implements OnInit {
   ids: Array<number>;
   flag = -1;
 
-
   sortOrder = true;
-  sortBy: 'merchandiser_code';
-  constructor(private httpService: DashboardService, private toastr: ToastrService, private excelService: ExcelService) {
+  sortBy: "merchandiser_code";
+
+  totalEvaluated = 0;
+  tmpTotal = 0;
+
+  constructor(
+    private httpService: DashboardService,
+    private toastr: ToastrService,
+    private excelService: ExcelService
+  ) {
     this.ids = [1, 2, 3, 4, 5, 6, 7];
-    this.zones = JSON.parse(localStorage.getItem('zoneList'));
+    this.zones = JSON.parse(localStorage.getItem("zoneList"));
   }
 
   ngOnInit() {
     this.loadingData = false;
-    this.userTypeId = localStorage.getItem('user_type');
-    this.ReEvaluatorId = localStorage.getItem('Reevaluator');
-    this.sortIt('merchandiser_code');
+    this.userTypeId = localStorage.getItem("user_type");
+    this.ReEvaluatorId = localStorage.getItem("Reevaluator");
+    this.sortIt("merchandiser_code");
     this.loadEvaluators();
     this.getMerchandiserList();
   }
-
-
 
   getMerchandiserList() {
     this.loadingData = true;
@@ -64,18 +68,18 @@ export class MerchandiserRosterComponent implements OnInit {
     if (this.userTypeId == this.ReEvaluatorId) {
       this.obj = {
         evaluatorId: this.selectedEvaluator.id || -1,
-        startDate: moment(this.startDate).format('YYYY-MM-DD'),
-        endDate: moment(this.endDate).format('YYYY-MM-DD'),
-        merId: this.selectedId
+        startDate: moment(this.startDate).format("YYYY-MM-DD"),
+        endDate: moment(this.endDate).format("YYYY-MM-DD"),
+        merId: this.selectedId,
       };
     } else {
-    this.obj = {
-      evaluatorId: localStorage.getItem('user_id'),
-      startDate: moment(this.startDate).format('YYYY-MM-DD'),
-      endDate: moment(this.endDate).format('YYYY-MM-DD'),
-      merId: this.selectedId
-    };
-  }
+      this.obj = {
+        evaluatorId: localStorage.getItem("user_id"),
+        startDate: moment(this.startDate).format("YYYY-MM-DD"),
+        endDate: moment(this.endDate).format("YYYY-MM-DD"),
+        merId: this.selectedId,
+      };
+    }
 
     this.httpService.getMerchandiserRoaster(this.obj).subscribe((data: any) => {
       // console.log('merchandiser list for evaluation',data);
@@ -87,22 +91,22 @@ export class MerchandiserRosterComponent implements OnInit {
     });
   }
 
-
-
-
   zoneChange() {
     this.loadingData = true;
-      this.getMerchandiserList();
+    this.getMerchandiserList();
 
     this.httpService.getRegion(this.selectedZone.id).subscribe(
-      data => {
+      (data) => {
         const res: any = data;
         if (res) {
           this.regions = res;
         } else {
           this.loadingData = false;
           this.loading = false;
-          this.toastr.info('Something went wrong,Please retry', 'Connectivity Message');
+          this.toastr.info(
+            "Something went wrong,Please retry",
+            "Connectivity Message"
+          );
         }
 
         setTimeout(() => {
@@ -110,7 +114,7 @@ export class MerchandiserRosterComponent implements OnInit {
           this.loading = false;
         }, 500);
       },
-      error => {
+      (error) => {
         this.loadingData = false;
         this.loading = false;
       }
@@ -119,8 +123,10 @@ export class MerchandiserRosterComponent implements OnInit {
 
   getArrowType(key) {
     if (key === this.sortBy) {
-      return this.sortOrder ? 'arrow_upward' : 'arrow_downward';
-    } else { return ''; }
+      return this.sortOrder ? "arrow_upward" : "arrow_downward";
+    } else {
+      return "";
+    }
   }
   sortIt(key) {
     this.sortBy = key;
@@ -129,11 +135,15 @@ export class MerchandiserRosterComponent implements OnInit {
 
   loadEvaluators() {
     this.httpService.getEvaluatorList().subscribe(
-      data => {
-        if (data) { this.evaluatorList = data; }
+      (data) => {
+        if (data) {
+          this.evaluatorList = data;
+        }
       },
-      error => {
-        error.status === 0 ? this.toastr.error('Please check Internet Connection', 'Error') : this.toastr.error(error.description, 'Error');
+      (error) => {
+        error.status === 0
+          ? this.toastr.error("Please check Internet Connection", "Error")
+          : this.toastr.error(error.description, "Error");
       }
     );
   }
@@ -143,7 +153,7 @@ export class MerchandiserRosterComponent implements OnInit {
     console.log(file, dataTable);
     const type = file.key;
     const data: any = dataTable;
-    const fileTitle = 'Merchandiser Roster';
+    const fileTitle = "Merchandiser Roster";
 
     this.excelService.exportAsExcelFile(data, fileTitle);
 
@@ -153,14 +163,20 @@ export class MerchandiserRosterComponent implements OnInit {
     }, 1000);
   }
 
-
   putValue(map, index) {
     for (const element of map) {
       // tslint:disable-next-line:triple-equals
       if (element.key == index) {
+        this.tmpTotal = this.tmpTotal + element.value;
         return element.value;
       }
     }
-    return '';
+    return "";
+  }
+
+  showTotal() {
+    this.totalEvaluated = this.tmpTotal;
+    this.tmpTotal = 0;
+    return this.totalEvaluated;
   }
 }
